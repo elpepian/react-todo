ec837e7f4c59a165c9bed34b45ba30a56b5ed693
elpepian <pp.vise@gmail.com>
2019-11-26 10:01:06 -0500

    Allowing /build folder in gitignore

---
+++ build/precache-manifest.2761b75f0b613aae66e50767eda3473d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b6b6ff0532e2c4fb3e89f337bdd8d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfb1f2c762eb4a07a24e",
+    "url": "/static/css/main.62e4047d.chunk.css"
+  },
+  {
+    "revision": "eef8b33f566bc8c38212",
+    "url": "/static/js/2.3283680c.chunk.js"
+  },
+  {
+    "revision": "dfb1f2c762eb4a07a24e",
+    "url": "/static/js/main.a88c7394.chunk.js"
+  },
+  {
+    "revision": "5bfcb64c3b7ccfa9e599",
+    "url": "/static/js/runtime-main.02dd13ee.js"
+  }
+]);
